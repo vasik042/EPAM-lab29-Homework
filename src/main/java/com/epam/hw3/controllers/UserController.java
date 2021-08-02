13eacfcdcf91650ace5a23728debc3f94e5e37eb
Vasil <vasik042@gmail.com>
2021-08-02 22:50:54 +0300

    Add logger to UserController

--- src/main/java/com/epam/hw3/controllers/UserController.java
+++ src/main/java/com/epam/hw3/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.epam.hw3.controllers;
 
 import com.epam.hw3.DTOs.UserDTO;
 import com.epam.hw3.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,32 +14,38 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     UserService userService;
+    Logger logger;
 
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+        logger = LoggerFactory.getLogger(UserController.class);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public UserDTO createUser(@RequestBody UserDTO userDTO) {
+        logger.info("Create user with email: " + userDTO.email);
         return userService.createUser(userDTO);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "/{email}", method = RequestMethod.GET)
     public UserDTO getUser(@PathVariable String email) {
+        logger.info("Get user with email: " + email);
         return userService.findUser(email);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "/{email}", method = RequestMethod.PUT)
     public UserDTO updateUser(@PathVariable String email, @RequestBody UserDTO userDTO) {
+        logger.info("Update user with email: " + email);
         return userService.updateUser(userDTO);
     }
 
     @RequestMapping(value = "/{email}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> deleteUser(@PathVariable String email) {
+        logger.info("Delete user with email: " + email);
         userService.deleteUser(email);
         return ResponseEntity.noContent().build();
     }
